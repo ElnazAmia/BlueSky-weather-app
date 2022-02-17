ab67b2877b88169e8cc0df21478e1bc45466a5d8
Elnaz <elnazamia@yahoo.com>
2022-02-17 15:23:22 +0100

    calculating time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
 function showTemp(response) {
   console.log(response.data);
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -5,16 +21,17 @@ function showTemp(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
-
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#city").innerHTML = response.data.name;
-
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
+  );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
   );
 }
 
 let apiKey = "f0ff64afa8957098b6eda5ad96796c19";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemp);
