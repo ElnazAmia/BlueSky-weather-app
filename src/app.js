02375e607fc63f759c6277799027358197fa5138
Elnaz <elnazamia@yahoo.com>
2022-02-19 17:19:03 +0100

    implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -14,13 +14,13 @@ function formatDate(timestamp) {
 }
 function showTemp(response) {
   console.log(response.data);
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  celisiusTemp = Math.round(response.data.main.temp);
+  document.querySelector("#temperature").innerHTML = celisiusTemp;
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#city").innerHTML = response.data.name;
+
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
@@ -34,8 +34,43 @@ function showTemp(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "f0ff64afa8957098b6eda5ad96796c19";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-let apiKey = "f0ff64afa8957098b6eda5ad96796c19";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=tabriz&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
 
-axios.get(apiUrl).then(showTemp);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+document.querySelector("#search-form").addEventListener("submit", handleSubmit);
+
+function displyToFahrenheit(event) {
+  event.preventDefault();
+  document.querySelector("#temperature").innerHTML = Math.round(
+    (celisiusTemp * 9) / 5 + 32
+  );
+
+  celisuisLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+}
+
+function displyToCelisius(event) {
+  event.preventDefault();
+  document.querySelector("#temperature").innerHTML = celisiusTemp;
+  celisuisLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+}
+
+celisiusTemp = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displyToFahrenheit);
+
+let celisuisLink = document.querySelector("#celisius-link");
+celisuisLink.addEventListener("click", displyToCelisius);
+
+search("New York");
